b38cf278e403f612a071a3ea0bf90bab47615caa
Jonathan Buhler <jonathan@buhler.dev>
2021-02-17 17:14:30 +0000

    Initial pass on Galaxy10 dataset

--- src/data.py
+++ src/data.py
@@ -1,35 +1,8 @@
-from astropy.io import fits
-from astropy.io.fits import hdu
-from astroquery.sdss import SDSS
-from astropy.coordinates import SkyCoord
-from astropy import units as u
-file = "./src/data/frame-r-002505-3-0038.fits"
-# fits.info(file)
-# image_data = fits.getdata(file, ext=1)
-# print(image_data.names)
-# # print(image_data.shape)
+from astroNN.datasets import galaxy10
 
-# with fits.open("./src/data/gz1.fits") as hdu_list:
-#     primary = hdu_list[1]
-#     print(primary.info())587-72717-8986356823
+# Downloads/loads dataset to/from ~/.astroNN/datasets
+# Corporate says to ignore the obnoxious environment warnings
+images, labels = galaxy10.load_data()
 
-# co = SkyCoord(ra="179.04298400878906", dec="60.522518157958984", unit="deg")
-co = SkyCoord('0h8m05.63s +14d50m23.3s')
-result = SDSS.query_region(co)
-print(result[:5])
-# xid = SDSS.query_region(co, spectro=True)
-
-# xid = SDSS.query_region(co)
-print(xid)
-hdu_list = SDSS.get_images(matches=co)
-primary = hdu_list[1][0].data
-# primary = hdu_list[0][0]
-# print(primary.data.shape)
-
-import matplotlib.pyplot as plt
-from astropy.visualization import astropy_mpl_style
-plt.style.use(astropy_mpl_style)
-plt.figure()
-plt.imshow(primary.data)
-plt.colorbar()
-plt.savefig("test.jpg")
+print(images)
+print(labels)
