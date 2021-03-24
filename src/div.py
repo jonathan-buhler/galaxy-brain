7ca3037277fe6cde332bd4b51114201db6d3b70d
Ion Bruckner <s1710295@ed.ac.uk>
2021-03-24 03:36:19 +0000

    wgan plots

--- src/div.py
+++ src/div.py
@@ -29,7 +29,7 @@ torch.manual_seed(SEED)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--n_epochs", type=int, default=200, help="number of epochs of training"
+    "--n_epochs", type=int, default=20, help="number of epochs of training"
 )
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
